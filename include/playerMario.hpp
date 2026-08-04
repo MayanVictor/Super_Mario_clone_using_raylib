0a968c605df277841156263559d4fb3701878f26
MayanVictor <mayanvctr@gmail.com>
2026-08-03 22:51:23 -0300

    Creating a Header For The Player
    - separating the Mario object into a header file

---
+++ include/playerMario.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <raylib.h>
+
+enum Direction { LEFT, RIGHT, NONE };
+class Mario {
+    public:
+        Mario();
+        void update(float deltaTime);
+        void jump();
+        void stopJump();
+        Direction setDirection(Direction dir);
+        Rectangle getHitbox();
+        bool IsInGround(bool inground);
+        bool headCollision(Rectangle head, Rectangle Block);
+        bool RightCollision(Rectangle playerright, Rectangle Block);
+        bool LeftCollision(Rectangle playerLeft, Rectangle Block);
+        void landOn(float floorY);
+        bool isFalling() const;
+        void draw();
+    private:
+        Vector2 position;
+        Vector2 velocity;
+        Rectangle Hitbox;
+        Direction direction;
+        int health;
+        float gravity;
+        float groundFriction;
+        float fallGravityMultiplier;
+        float maxMoveSpeed;
+        float acceleration;
+        float jumpSpeed ;
+        float maxFallSpeed;
+        bool inGround;
+};
